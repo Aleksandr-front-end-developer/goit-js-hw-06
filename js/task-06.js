6c81cc3600a6a532e517923f3da96d25efac5773
Oleksandr Danyliuk <oleksandr.danyliuk.88@gmail.com>
2023-11-17 12:38:09 +0200

    remove console.log

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,6 @@ const inputNeedLength = Number(input.dataset.length);
 
 input.addEventListener('blur', (event) => {
 	if (event.currentTarget.value.length === inputNeedLength) {
-		console.log('inputNeedLength', inputNeedLength)
-		console.log('event.currentTarget.value.length', event.currentTarget.value.length)
 		event.currentTarget.classList.remove('invalid');
 		event.currentTarget.classList.add('valid');
 	} else {
